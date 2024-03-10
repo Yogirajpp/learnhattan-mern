804a1cd8ef62e401bc2afe4382fdbc1de5b6de9b
Yogiraj Patil <yogiraj10033921@gmail.com>
2024-03-10 22:29:47 +0530

    add

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+<<<<<<< HEAD
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios"; // Import axios for making HTTP requests
 import { FaBars, FaGraduationCap, FaNewspaper, FaShoppingCart, FaTachometerAlt, FaSignOutAlt } from "react-icons/fa";
@@ -24,6 +25,27 @@ const Sidebar = () => {
       console.error("Logout failed:", error);
     }
   }
+=======
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import {
+  FaBars,
+  FaGraduationCap,
+  FaNewspaper,
+  FaShoppingCart,
+  FaTachometerAlt,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import "./Sidebar.css"; // Import your CSS file
+
+const Sidebar = () => {
+  const { logout } = useLogout();
+  const [isOpen, setIsOpen] = useState(true);
+
+  const SignOut = async () => {
+    logout();
+  };
+>>>>>>> ee6eb2a5cce590c78ce894e29a23075ab6ed8848
 
   return (
     <div className={`sideMenu ${isOpen ? "open" : ""}`}>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthContextProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <AuthContextProvider>

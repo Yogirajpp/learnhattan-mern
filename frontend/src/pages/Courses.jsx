5519ec2cbd97251aefa8e391ca4e6fe40785c6a2
yashj09 <yashj8858@gmail.com>
2024-03-16 05:01:28 +0530

    Update Courses.jsx

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -33,31 +33,31 @@ const Courses = () => {
           <div className="md:col-span-2 ml-48">
             <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
               {courses.map((course) => (
-                <div key={course._id}>
-                  <div className="flex flex-col rounded-lg overflow-hidden">
-                    <img
-                      alt="Course thumbnail"
-                      className="aspect-[16/9]"
-                      height={225}
-                      src="/assets/images/img6.png"
-                      width={400}
-                    />
-                    <div className="p-4 flex-1">
-                      <h2 className="text-lg font-semibold">{course.title}</h2>
-                      <p className="text-sm text-gray-500 line-clamp-2">
-                        Description: {course.description}{" "}
-                      </p>
-                      <p className="text-sm text-gray-500 mt-2">
-                        Category: {course.category}
-                      </p>
-                    </div>
-                    <button
-                      className="flex items-center justify-center p-4 bg-gray-100 text-sm w-full"
-                      onClick={() => navigate(`/coursedetail/${course._id}`)}
-                    >
-                      View Details
-                    </button>
+                <div
+                  key={course._id}
+                  className="flex flex-col shadow-lg rounded-3xl overflow-hidden"
+                >
+                  <img
+                    alt="Course thumbnail"
+                    height={225}
+                    src="/assets/images/img6.png"
+                    width={410}
+                  />
+                  <div className="p-4 flex-1">
+                    <h2 className="text-lg font-semibold">{course.title}</h2>
+                    <p className="text-sm text-gray-500 line-clamp-2">
+                      {course.description}{" "}
+                    </p>
+                    <p className="text-sm text-gray-500 mt-2">
+                      {course.category}
+                    </p>
                   </div>
+                  <button
+                    className="flex items-center justify-center p-4 bg-gray-100 text-sm w-full"
+                    onClick={() => navigate(`/coursedetail/${course._id}`)}
+                  >
+                    View Details
+                  </button>
                 </div>
               ))}
             </div>

e65bf3988c47a708eb8094b9f36b008eb9b4ed80
yashj09 <yashj8858@gmail.com>
2024-03-30 10:51:02 +0530

    new WalletConnectButton component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useLogIn } from '../hooks/useLogin';
+// import { ConnectButton } from '@rainbow-me/rainbowkit';
+import WalletConnectButton from '@/components/WalletConnectButton';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -65,6 +67,9 @@ const Login = () => {
           >
             Log-In
           </button>
+          <div className="px-5 m-1 flex">
+            <WalletConnectButton />
+          </div>
           {error &&
             <div>
               {error}
